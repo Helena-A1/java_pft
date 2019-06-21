75debd1c5accbbacfc8b8cd073c63db1677b62ed
Alena_Alshevskaya <helena.softwaretest@gmail.com>
2019-06-21 14:49:25 +0300

    refactor class

--- java_pft_sandbox/src/main/java/MyFirstProgram.java
+++ java_pft_sandbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,7 @@
 public class MyFirstProgram {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-System.out.println(" Hellow, world");
-}}
+        System.out.println(" Hellow, world");
+    }
+}

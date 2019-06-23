c418728abe9244f015da58621430465fbbcf54ef
Alena_Alshevskaya <helena.softwaretest@gmail.com>
2019-06-24 01:12:01 +0300

    area calculations

--- java_pft_sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ java_pft_sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,13 +8,15 @@ public class MyFirstProgram {
         hello("User");
         hello("Alena");
 
-        double l = 5;
-        System.out.println("Площадь квадрата  сo стороной  " + l + " = " + area(l));
+        Square s = new Square(5);
+
+      System.out.println( " Площадь квадрата сo стороной " + s.l + " = " + area(s));
 
 
-        double a = 2;
-        double b = 4;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a , b));
+
+Rectangle r = new Rectangle(4,6);
+
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 
     }
     public static void hello(String somebody) {
@@ -22,11 +24,11 @@ public class MyFirstProgram {
         System.out.println(" Hello " + somebody + "!");
     }
 
-    public static double area(double len) {
-        return len*len;
+    public static double area(Square s) {
+        return s.l*s.l;
     }
 
-    public static double area(double a, double b){
-        return a * b;
+    public static double area(Rectangle r){
+        return r.a * r.b;
     }
 }

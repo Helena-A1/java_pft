f790ab02adaef0290b9f31ade0cc5f0cba733b08
Alena_Alshevskaya <helena.softwaretest@gmail.com>
2019-06-24 00:46:47 +0300

    functions

--- java_pft_sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ java_pft_sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,25 +4,29 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        System.out.println(" Hello, world");
+        hello("world");
+        hello("User");
+        hello("Alena");
 
-        System.out.println(2+2);
-        System.out.println(2*2);
-        System.out.println(2 / 2);
-        System.out.println(2 - 2);
-
-
-        System.out.println(1/2);
-        System.out.println(1.0 / 2);
-        System.out.println(2/ 2.0);
-
-        System.out.println("2" + "2");
-        System.out.println("2" + 2);
+        double l = 5;
+        System.out.println("Площадь квадрата  сo стороной  " + l + " = " + area(l));
 
 
-        System.out.println( (2 + 2)*2);
+        double a = 2;
+        double b = 4;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a , b));
 
+    }
+    public static void hello(String somebody) {
 
-        System.out.println( "2 +2 = " + 2 + 2);
+        System.out.println(" Hello " + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len*len;
+    }
+
+    public static double area(double a, double b){
+        return a * b;
     }
 }

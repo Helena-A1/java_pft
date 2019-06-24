667694ee0ad838e0794dc89b1cac0e75a465e27e
Alena_Alshevskaya <helena.softwaretest@gmail.com>
2019-06-24 14:42:33 +0300

    задание 2

--- java_pft_sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ java_pft_sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,31 +2,37 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        hello("world");
-        hello("User");
-        hello("Alena");
+    hello("world");
+    hello("User");
+    hello("Alena");
 
-        Square s = new Square(5);
+    Square s = new Square(5);
+    System.out.println(" Площадь квадрата сo стороной " + s.l + " = " + s.area());
 
-      System.out.println( " Площадь квадрата сo стороной " + s.l + " = " + s.area());
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
+    // Задание №2: Потренироваться использовать функции, классы, объекты и методы
+    //  Объявление точек
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(3, 4);
 
+    System.out.println(" Тoчка p1 имеет координаты " + p1.x + " и " + p1.y);
+    System.out.println(" Тoчка p2 имеет координаты " + p2.x + " и " + p2.y);
 
-Rectangle r = new Rectangle(4,6);
+    // использование функции для нахождения расстояния
+    System.out.println(" Расстояние между точками равно " + Point.distance(p1,p2));
 
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    // использование метода для нахождения расстояния
+    System.out.println(" Расстояние между точками равно " + p1.distance(p2));
 
-    }
-    public static void hello(String somebody) {
+  }
 
-        System.out.println(" Hello " + somebody + "!");
-    }
+  public static void hello(String somebody) {
 
+    System.out.println(" Hello " + somebody + "!");
+  }
 
-
-    public static double area(Rectangle r){
-        return r.a * r.b;
-    }
-}
+  }

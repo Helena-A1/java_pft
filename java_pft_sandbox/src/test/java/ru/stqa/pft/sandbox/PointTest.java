4394ff37ef278adb50d63b9b94f36b416fa965f8
Alena_Alshevskaya <helena.softwaretest@gmail.com>
2019-06-25 17:04:04 +0300

    Point tests

--- java_pft_sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ java_pft_sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -11,10 +11,26 @@ public class PointTest {
 
 //check that the distance value equals expected result
    Assert.assertEquals(Point.distance(p1, p2), 5.0);
+ }
 
-   //check that the distance from p1 to p2 equals the backwards distance
+  //check that the distance from p1 to p2 equals the backwards distance
+ @Test
+   public  void testBackDistance () {
+   Point p1 = new Point(0, 0);
+   Point p2 = new Point(3, 4);
+
    Assert.assertEquals(Point.distance(p1, p2), Point.distance(p2, p1));
+ }
 
+
+   //check that the distance from p1 to p1
+
+  @Test
+  public  void testOnePoint () {
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(3, 4);
+
+    Assert.assertEquals(Point.distance(p1, p1), 0.0);
  }
 
 

84b75ca6f6e4d82909a73407dda519c7dd046acd
Alena_Alshevskaya <helena.softwaretest@gmail.com>
2019-07-04 15:36:32 +0300

    private ContactHelper contactHelper ia added

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,70 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+  public WebDriver wd;
+
+  private SessionHelper sessionHelper;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+  private ContactHelper contactHelper;
+
+  public void init() {
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+    contactHelper = new ContactHelper(wd);
+    sessionHelper.login("user", "admin", "pass", "secret", By.xpath("//input[@value='Login']"));
+  }
+
+
+
+  public void stop() {
+     wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+       wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+       wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
+  }
+
+  public ContactHelper getContactHelper() {
+    return contactHelper;
+  }
+
+
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
+
+  public WebDriver getWd() {
+    return wd;
+  }
+}

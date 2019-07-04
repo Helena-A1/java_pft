431d10d16cd179013d459b5b8b42e4a53843f570
Alena_Alshevskaya <helena.softwaretest@gmail.com>
2019-07-04 15:58:12 +0300

    refactoring of TestBase class

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,5 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
@@ -7,16 +11,39 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 public class TestBase {
 
   protected final ApplicationManager app = new ApplicationManager();
+  private WebDriver wd;
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
     app.init();
   }
 
+
+
+
   @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
 
   }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
 
 }

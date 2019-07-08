cc1f009b55d16378d8ed6fa2122c09203200caf6
Alena_Alshevskaya <helena.softwaretest@gmail.com>
2019-07-08 19:37:12 +0300

    SessionHelper extends HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -3,21 +3,18 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SessionHelper {
-  public WebDriver wd;
+public class SessionHelper extends HelperBase {
+
 
   public SessionHelper(WebDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void login(String user, String admin, String pass, String secret, By xpath) {
-    wd.findElement(By.name(user)).click();
-    wd.findElement(By.name(user)).clear();
-    wd.findElement(By.name(user)).sendKeys(admin);
-    wd.findElement(By.name(pass)).click();
-    wd.findElement(By.name(pass)).clear();
-    wd.findElement(By.name(pass)).sendKeys(secret);
-    wd.findElement(xpath).click();
+    type(By.name(user), admin);
+    type(By.name(pass), secret );
+    click(xpath);
+
   }
 }
 

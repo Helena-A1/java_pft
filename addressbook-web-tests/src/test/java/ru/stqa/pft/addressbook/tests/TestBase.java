78854b77a526a8ea04ade50ccaa1ab9f2eb98e2a
Alena_Alshevskaya <helena.softwaretest@gmail.com>
2019-07-08 11:56:08 +0300

    TestBase class

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,14 +1,14 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
-  private WebDriver wd;
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {

c7db10eaa738414701f755a88473dbee0b6cabf3
Alena_Alshevskaya <helena.softwaretest@gmail.com>
2019-08-17 23:37:39 +0300

    Sighup + Получение почты на встроенный почтовый сервер

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -19,6 +19,7 @@ public class ApplicationManager {
   private String browser;
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
+  private MailHelper mailHelper;
 
 
   public ApplicationManager(String browser) {
@@ -79,6 +80,14 @@ public class ApplicationManager {
     }
     return wd;
   }
+
+  public MailHelper mail(){
+    if (mailHelper == null) {
+      mailHelper = new MailHelper(this);
+    }
+    return mailHelper;
+    }
+
 }
 
 
